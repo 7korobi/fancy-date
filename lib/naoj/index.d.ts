af8eaabceb4cb955ee6f091193ec0da6aa17266b
7korobi <7korobi@gmail.com>
2026-07-08 02:34:47 +0900

    format.

--- lib/naoj/index.d.ts
+++ lib/naoj/index.d.ts
@@ -1,16 +1,3 @@
-export type {
-  LunarEquatorialCoordinates,
-  LunarApsis,
-  LunarApsisKind,
-  LunarHorizontalCoordinates,
-  LunarNode,
-  LunarNodeKind,
-  LunarObservation,
-  LunarObservationOptions,
-  SolarEquatorialCoordinates,
-  SolarHorizontalCoordinates,
-  SolarObservation,
-  SolarObservationOptions,
-} from '../orbital-model'
-export * from './earth-solar'
-export * from './earth-moon'
+export type { LunarEquatorialCoordinates, LunarApsis, LunarApsisKind, LunarHorizontalCoordinates, LunarNode, LunarNodeKind, LunarObservation, LunarObservationOptions, SolarEquatorialCoordinates, SolarHorizontalCoordinates, SolarObservation, SolarObservationOptions, } from '../orbital-model';
+export * from './earth-solar';
+export * from './earth-moon';
--- lib/nasa/mars-solar.d.ts
+++ lib/nasa/mars-solar.d.ts
@@ -1,22 +1,22 @@
-import type { BodyProfile, PLANET, ROTATION, STAR } from '../orbital-model'
+import type { BodyProfile, PLANET, ROTATION, STAR } from '../orbital-model';
 export type MarsSolarOrbitalOptions = {
-  periodMsec?: number
-  epochMsec?: number
-  body?: BodyProfile
-}
+    periodMsec?: number;
+    epochMsec?: number;
+    body?: BodyProfile;
+};
 export declare class MarsSolarOrbital {
-  static readonly sun: STAR
-  static readonly meanSolarDayMsec = 88775244
-  static readonly rotationEpochMsec = 0
-  static readonly axialTiltDeg = 25.19
-  static readonly meanTropicalYearMsec = 59355072000
-  static readonly vernalEquinoxEpochMsec: number
-  readonly periodMsec: number
-  readonly epochMsec: number
-  constructor({ periodMsec, epochMsec }?: MarsSolarOrbitalOptions)
-  static rotation(): ROTATION
-  static planet(center?: STAR, options?: MarsSolarOrbitalOptions): PLANET
-  phaseAt(utc: number): number
-  timeOfPhase(phase: number, near: number): number
-  solarLongitudeDeg(utc: number): number
+    static readonly sun: STAR;
+    static readonly meanSolarDayMsec = 88775244;
+    static readonly rotationEpochMsec = 0;
+    static readonly axialTiltDeg = 25.19;
+    static readonly meanTropicalYearMsec = 59355072000;
+    static readonly vernalEquinoxEpochMsec: number;
+    readonly periodMsec: number;
+    readonly epochMsec: number;
+    constructor({ periodMsec, epochMsec, }?: MarsSolarOrbitalOptions);
+    static rotation(): ROTATION;
+    static planet(center?: STAR, options?: MarsSolarOrbitalOptions): PLANET;
+    phaseAt(utc: number): number;
+    timeOfPhase(phase: number, near: number): number;
+    solarLongitudeDeg(utc: number): number;
 }
